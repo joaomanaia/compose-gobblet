acb7870877008b22d5d67f13cdaf3f2b23e466c4
joaomanaia <joaopfmanaia@gmail.com>
2024-04-01 19:02:18 +0100

    Fix dynamic color when using light theme in GobbletTheme

--- composeApp/src/androidMain/kotlin/core/presentation/theme/Theme.android.kt
+++ composeApp/src/androidMain/kotlin/core/presentation/theme/Theme.android.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
@@ -22,7 +23,7 @@ actual fun GobbletTheme(
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
 
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicDarkColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
         darkTheme -> DarkThemeColors
         else -> LightThemeColors
